6a85b6299bea46f8bcb0cc3cffeecd9c61345598
Shane <shane.barnwell@overstock.com>
2017-01-20 13:26:11 -0700

    adding file for bookmarklet

--- routes/index.js
+++ routes/index.js
@@ -31,31 +31,4 @@ router.get('/', function(req, res, next) {
     });
 });
 
-// USE THIS AS A BOOKMARKLET IN CHROME:
-// javascript:
-//     var descriptionText = document.getElementById('descriptionmodule');
-//     var descriptionParts = descriptionText.innerText.split(/[\s\n]+/);
-//     var prodUrl;
-//     descriptionParts.forEach(function(item) {
-//         if (item.indexOf('/www/') > -1) {
-//             prodUrl = item.replace('/www/', 'https://ak1.ostkcdn.com/');
-//         }
-//     });
-//     jQuery.get('https://asset-in-prod.herokuapp.com/?url=' + encodeURIComponent(prodUrl))
-//     .done(function (data) {
-//         var html = '';
-//             html += '<b>status:</b> ' + data.status + '<br>';
-//             html += '<b>path:</b> ' + data.path + '<br>';
-//             if (data.status === 200) {
-//                 html += '<b>md5:</b> ' + data.md5 + '<br>';
-//                 html += '<b>length:</b> ' + data.length + '<br>';
-//             }
-//         jQuery.fancybox({
-//             'content': html,
-//             'width': 500,
-//             'autoDimensions':false,
-//             'autoSize': false
-//         });
-//     });
-
 module.exports = router;
