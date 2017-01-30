9d20b38cd505e28783588c7734baf1ec7e3b2f4c
Shane <shane.barnwell@overstock.com>
2017-01-30 11:46:45 -0700

    updated messaging and styles

--- public/client.js
+++ public/client.js
@@ -10,16 +10,21 @@
             }
         });
         jQuery.get('https://asset-in-prod.herokuapp.com/?url=' + encodeURIComponent(prodUrl)).done(function(data) {
+            var styleTag = "<style>"
             var html = '';
-            var deployMessage = 200 === data.status ? "DEPLOYED TO PROD! BOOM BOOM!" : "NOT DEPLOYED";
-            html += '<b>Path:</b> ' + data.path + '<br>';
-            html += "<b>Deploy Status:</b> " + deployMessage + '<br><hr>';
-            if (data.status === 200) {
-                html += '(' + data.md5 + ')';
+            var deployMessage = 'DEPLOYED TO PROD!';
+            var statusColor = '#118c01';
+            if (data.status !== 200) {
+                deployMessage = 'NOT ' + deployMessage;
+                statusColor = '#a30000';
             }
+            html += '<div style="font-weight: bold;font-size: 24px;border-bottom: 1px solid lightgrey;padding-bottom: 20px;margin: 20px 0;color:' + statusColor + ';">' + deployMessage + '</div>';
+            html += '<div style="color: #484848;"><strong style="width: 85px; display: inline-block; padding-bottom: 5px;">Path:</strong> ' + data.path + '</div>';
+            html += '<div style="color: #484848;"><strong style="width: 85px; display: inline-block;">Prod md5:</strong> ' + data.md5 + '</div>';
             jQuery.fancybox({
                 'content': html,
                 'width': 500,
+                'height': 150,
                 'autoDimensions': false,
                 'autoSize': false
             });
